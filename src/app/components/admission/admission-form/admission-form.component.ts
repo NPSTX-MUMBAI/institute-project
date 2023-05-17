76ea2e58262dc8898013cfaa86b1c77bde2d324b
ShivaniS1999 <rsinghshivani55@gmail.com>
2023-05-17 12:02:10 +0530

    student details form

--- src/app/components/admission/admission-form/admission-form.component.ts
+++ src/app/components/admission/admission-form/admission-form.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { log } from 'console';
+import { MenuItem } from 'primeng/api';
 
 @Component({
-  selector: 'app-admission-form',
-  templateUrl: './admission-form.component.html',
-  styleUrls: ['./admission-form.component.scss']
+    selector: 'app-admission-form',
+    templateUrl: './admission-form.component.html',
+    styleUrls: ['./admission-form.component.scss'],
 })
-export class AdmissionFormComponent {
+export class AdmissionFormComponent implements OnInit {
+    items!: MenuItem[];
+    index: any = 0;
 
+    activeItem!: MenuItem;
+
+    ngOnInit() {
+        this.items = [
+            { index: 0, label: 'Student Details', icon: 'pi pi-fw pi-user' },
+            { index: 1, label: 'Parents Details', icon: 'pi pi-fw pi-user' },
+            { index: 2, label: 'Kyc Details', icon: 'pi pi-fw pi-pencil' },
+        ];
+
+        this.activeItem = this.items[0];
+    }
+
+    onActiveItemChange(event: MenuItem) {
+        console.log(event);
+        this.activeItem = event;
+        this.index = event.index;
+    }
+
+    activateLast() {
+        this.activeItem = this.items[this.items.length - 1];
+    }
 }
--- src/app/components/admission/admission.module.ts
+++ src/app/components/admission/admission.module.ts
@@ -3,11 +3,19 @@ import { CommonModule } from '@angular/common';
 import { AdmissionFormComponent } from './admission-form/admission-form.component';
 import { Routes, RouterModule } from '@angular/router';
 import { UiModule } from '../../ui/ui.module';
+import { ParentDetailsComponent } from './admission-form/parent-details/parent-details.component';
+import { KycDetailsComponent } from './admission-form/kyc-details/kyc-details.component';
+import { StudentDetailsComponent } from './admission-form/student-details/student-details.component';
 
 const routes: Routes = [{ path: '', component: AdmissionFormComponent }];
 
 @NgModule({
-    declarations: [AdmissionFormComponent],
+    declarations: [
+        AdmissionFormComponent,
+        StudentDetailsComponent,
+        ParentDetailsComponent,
+        KycDetailsComponent,
+    ],
     imports: [CommonModule, RouterModule.forChild(routes), UiModule],
 })
 export class AdmissionModule {}
