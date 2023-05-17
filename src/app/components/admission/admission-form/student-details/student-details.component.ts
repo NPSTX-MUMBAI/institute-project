0a248d7e3606928b8ec8d78223bb2e22754b1f4d
ShivaniS1999 <rsinghshivani55@gmail.com>
2023-05-17 17:03:25 +0530

    student form validation

--- src/app/components/admission/admission-form/student-details/student-details.component.ts
+++ src/app/components/admission/admission-form/student-details/student-details.component.ts
@@ -1,8 +1,29 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
     selector: 'app-student-details',
     templateUrl: './student-details.component.html',
     styleUrls: ['./student-details.component.scss'],
 })
-export class StudentDetailsComponent {}
+export class StudentDetailsComponent {
+    value5: any;
+
+    studentDetails = new FormGroup({
+        firstname: new FormControl('', Validators.required),
+        lastname: new FormControl('', Validators.required),
+        email: new FormControl('', Validators.required),
+        phone: new FormControl('', Validators.required),
+        address: new FormControl('', Validators.required),
+        dob: new FormControl('', Validators.required),
+        nationality: new FormControl('', Validators.required),
+        religion: new FormControl('', Validators.required),
+        birthPlace: new FormControl('', Validators.required),
+        state: new FormControl('', Validators.required),
+        Std: new FormControl('', Validators.required),
+        gender: new FormControl('', Validators.required),
+    });
+    submit() {
+        console.log(this.studentDetails.value);
+    }
+}
