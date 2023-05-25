2b127ea0b29a4b9da36580b5febde10aa3a47e8e
Shoaib <shoaib.salim@npstx.com>
2023-05-25 12:17:33 +0530

    feat: bank info, staff info

--- src/app/auth/signup/bank-info/bank-info.component.ts
+++ src/app/auth/signup/bank-info/bank-info.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {
+    FormBuilder,
+    FormControl,
+    FormGroup,
+    Validators,
+} from '@angular/forms';
 
 @Component({
-  selector: 'app-bank-info',
-  templateUrl: './bank-info.component.html',
-  styleUrls: ['./bank-info.component.scss']
+    selector: 'app-bank-info',
+    templateUrl: './bank-info.component.html',
+    styleUrls: ['./bank-info.component.scss'],
 })
-export class BankInfoComponent {
-
+export class BankInfoComponent implements OnInit {
+    bankDetails!: FormGroup;
+    constructor(private fb: FormBuilder) {}
+    ngOnInit(): void {
+        this.bankDetails = this.fb.group({
+            name: ['', Validators.required],
+            accountType: ['', Validators.required],
+            ifscCode: ['', Validators.required],
+            Accountno: ['', Validators.required],
+            upi: ['', Validators.required],
+            SID: ['', Validators.required],
+            mid: ['', Validators.required],
+            Bankname: ['', Validators.required],
+            Branchname: ['', Validators.required],
+            state: ['', Validators.required],
+            district: ['', Validators.required],
+            city: ['', Validators.required],
+            center: ['', Validators.required],
+            address: ['', Validators.required],
+            Bankcode: ['', Validators.required],
+        });
+    }
+    dropdownItems = [
+        { name: 'Current account', code: 'Current account' },
+        { name: 'Saving account', code: 'Saving account' },
+        { name: 'Salary accoount', code: 'Salary accoount' },
+        { name: 'FD accoount', code: 'FD  accoount' },
+    ];
+    submit() {
+        console.log(this.bankDetails.value);
+    }
 }
--- src/app/auth/signup/staff-info/staff-info.component.ts
+++ src/app/auth/signup/staff-info/staff-info.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {
+    FormBuilder,
+    FormControl,
+    FormGroup,
+    Validators,
+} from '@angular/forms';
 
 @Component({
-  selector: 'app-staff-info',
-  templateUrl: './staff-info.component.html',
-  styleUrls: ['./staff-info.component.scss']
+    selector: 'app-staff-info',
+    templateUrl: './staff-info.component.html',
+    styleUrls: ['./staff-info.component.scss'],
 })
-export class StaffInfoComponent {
+export class StaffInfoComponent implements OnInit {
+    staffDetails!: FormGroup;
 
+    constructor(private fb: FormBuilder) {}
+
+    ngOnInit(): void {
+        this.staffDetails = this.fb.group({
+            firstName: ['', Validators.required],
+            lastName: ['', Validators.required],
+            email: ['', Validators.required],
+            mobileNo: ['', Validators.required],
+            password: ['', Validators.required],
+            pin: ['', Validators.required],
+        });
+    }
+    submit() {
+        console.log(this.staffDetails.value);
+    }
 }
