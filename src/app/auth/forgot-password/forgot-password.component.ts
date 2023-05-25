7eb3bcb15eda734061359c1cdfebd5ec756dcbea
Shoaib <shoaib.salim@npstx.com>
2023-05-25 16:55:20 +0530

    feat:forgot-pw, login, new pass

--- src/app/auth/forgot-password/forgot-password.component.ts
+++ src/app/auth/forgot-password/forgot-password.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-forgot-password',
-  templateUrl: './forgot-password.component.html',
-  styleUrls: ['./forgot-password.component.scss']
+    selector: 'app-forgot-password',
+    templateUrl: './forgot-password.component.html',
+    styleUrls: ['./forgot-password.component.scss'],
 })
 export class ForgotPasswordComponent {
+    constructor(private router: Router) {}
 
+    submit() {
+        this.router.navigate(['auth/otp']);
+    }
 }
--- src/app/auth/new-password/new-password.component.ts
+++ src/app/auth/new-password/new-password.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-new-password',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./new-password.component.scss']
 })
 export class NewPasswordComponent {
+
+    constructor(private router: Router){}
+
+    submit() {
+        this.router.navigate(['auth/new-password']);
+    }
 
 }
