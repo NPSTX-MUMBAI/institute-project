7b7510aae0b000a4425e39f23f1c1050893bf51e
Shoaib <shoaib.salim@npstx.com>
2023-05-25 15:31:07 +0530

    feat: list of inst

--- src/app/components/institute/list-institute/list-institute.component.ts
+++ src/app/components/institute/list-institute/list-institute.component.ts
@@ -17,6 +17,59 @@ export class ListInstituteComponent implements OnInit {
     listInstitute!: FormGroup;
     //dropdownItems=[]
 
+    school: any = [
+        {
+            name: 'ABC School',
+            uniqueId: 1000,
+            type: 'Primary',
+            city: 'Example City',
+            pincode: '12345',
+        },
+        {
+            name: 'XYZ School',
+            uniqueId: 2001,
+            type: 'Secondary',
+            city: 'Another City',
+            pincode: '54321'
+          },{
+            name: 'PQR School',
+            uniqueId: 3002,
+            type: 'High School',
+            city: 'Some City',
+            pincode: '98765'
+          }, {
+            name: 'LMN School',
+            uniqueId: 4003,
+            type: 'Elementary',
+            city: 'Different City',
+            pincode: '24680'
+          },{
+            name: 'EFG School',
+            uniqueId: 5004,
+            type: 'Middle School',
+            city: 'New City',
+            pincode: '13579'
+          },{
+            name: 'GHI School',
+            uniqueId: 6005,
+            type: 'Public',
+            city: 'Cityville',
+            pincode: '86420'
+          },{
+            name: 'JKL School',
+            uniqueId: 7006,
+            type: 'Private',
+            city: 'Townsville',
+            pincode: '97531'
+          },{
+            name: 'MNO School',
+            uniqueId: 8007,
+            type: 'International',
+            city: 'Global City',
+            pincode: '75319'
+          }
+    ];
+
     constructor(private fb: FormBuilder, private router: Router) {}
     ngOnInit(): void {
         this.listInstitute = this.fb.group({
