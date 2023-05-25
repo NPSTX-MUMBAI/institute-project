442fe8bc9510928a95dd8e1fc33c9252fc5fc9cf
Shoaib <shoaib.salim@npstx.com>
2023-05-25 11:37:35 +0530

    feat: personal info comp

--- src/app/auth/signup/personal-info/personal-info.component.ts
+++ src/app/auth/signup/personal-info/personal-info.component.ts
@@ -1,10 +1,57 @@
-import { Component } from '@angular/core';
-
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 @Component({
-  selector: 'app-personal-info',
-  templateUrl: './personal-info.component.html',
-  styleUrls: ['./personal-info.component.scss']
+    selector: 'app-personal-info',
+    templateUrl: './personal-info.component.html',
+    styleUrls: ['./personal-info.component.scss'],
 })
-export class PersonalInfoComponent {
+export class PersonalInfoComponent implements OnInit {
+    personalGrp: FormGroup;
+    constructor(private fb: FormBuilder) {
+        this.personalGrp = fb.group({
+            firstName: ['', Validators.required],
+            lastName: ['', Validators.required],
+            email: [
+                '',
+                [
+                    Validators.required,
+                    Validators.pattern(
+                        '^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$'
+                    ),
+                ],
+            ],
+            mobileNo: [
+                '',
+                [Validators.required, Validators.pattern('^[0-9]*$')],
+            ],
+            password: [
+                null,
+                Validators.compose([
+                    Validators.required,
+                    Validators.pattern(
+                        /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/
+                    ),
+                    Validators.minLength(8),
+                ]),
+            ],
+            confirmPassword: [null, [Validators.required]],
+        });
+    }
 
+    ngOnInit(): void {}
+
+    onSave() {
+        //     try {
+        //         if (this.personalGrp.invalid) {
+        //             this.commanSrv.InvalidForm(this.personalGrp);
+        //         } else {
+        //             let user: any = this.personalGrp.value;
+        //             delete user.confirmPassword;
+        //             this.onSavePersonalInfo.emit(this.personalGrp.value);
+        //         }
+        //     } catch (error) {
+        //         this.messageService.showError(error.msg);
+        //     }
+        console.log('this.personalGrp.value');
+    }
 }
