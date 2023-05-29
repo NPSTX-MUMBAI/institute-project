7f85d135f5d550050e359e9bd803a528059fd73c
Shoaib <shoaib.salim@npstx.com>
2023-05-29 11:35:46 +0530

    feat: validation on login

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { AuthService } from '../../services/auth.service';
+import { MessageService } from 'primeng/api';
 
 @Component({
     selector: 'app-login',
@@ -11,7 +12,12 @@ import { AuthService } from '../../services/auth.service';
 export class LoginComponent implements OnInit {
     loginGrp!: FormGroup;
 
-    constructor(private router: Router, private fb: FormBuilder, private auth: AuthService) {}
+    constructor(
+        private router: Router,
+        private fb: FormBuilder,
+        private auth: AuthService,
+        private msgSvc: MessageService
+    ) {}
 
     ngOnInit(): void {
         this.loginGrp = this.fb.group({
@@ -27,10 +33,106 @@ export class LoginComponent implements OnInit {
 
     signin() {
         console.log(this.loginGrp.value);
+        if (this.loginGrp.invalid) {
+            this.msgSvc.add({
+                severity: 'danger',
+                summary: 'incorrect data',
+                detail: 'Enter proper email and password',
+            });
+        } else {
+            console.log('successsss');
+        }
 
-
-
-        
         // this.router.navigate(['admin-dashboard']);
     }
+
+    // login() {
+    //     if (this.loginForm.invalid) {
+    //       this.messageService.showError("Enter Mobile Number And Password")
+    //     }
+    //     else {
+    //       this.authService.loginUser(this.loginForm.value).then((res: any) => {
+    //         console.log(res)
+    //         if (res.data.userType == USER_TYPE.CUSTOMER) {
+    //           this.authService.setUserData("user", res.data)
+    //           this.authService.setUserData("accessToken", res.tokens.accessToken)
+    //           this.authService.setUserData("refreshToken", res.tokens.refreshToken)
+    //           this.router.navigate(['/dashboard/customer-dashboard'])
+    //           return
+    //         }
+    //         else if (res.data.isRegistrationComplete && res.data.isApproved) {
+    //           this.authService.setUserData("user", res.data)
+    //           this.authService.setUserData("accessToken", res.tokens.accessToken)
+    //           this.authService.setUserData("refreshToken", res.tokens.refreshToken)
+    //           this.router.navigate(['/dashboard/merchant-dashboard'])
+    //           return
+    //         } else {
+    //           if (res.data.isRegistrationComplete && !res.data.isApproved) {
+    //             let userData = { userId: res.data.userId, businessId: res.data.businessId, businessBankId: res.data.businessBankId, businessKycId: res.data.businessKycId }
+    //             this.authService.setUserData('user', userData)
+    //             this.router.navigate(['/auth/signup'])
+    //             return
+    //           } else if (!res.data.isRegistrationComplete) {
+    //             let userId = { userId: res.data.userId }
+    //             this.authService.setUserData('user', userId)
+    //             if (!res.data.businessId) {
+    //               this.messageService.showInfo(res.msg)
+    //               this.router.navigate(['/auth/signup'])
+    //               return
+    //             }
+    //             let userData = this.authService.getUserData('user')
+    //             let businessId = { ...userData, businessId: res.data.businessId }
+    //             this.authService.setUserData('user', businessId)
+    //             if (!res.data.businessBankId) {
+    //               this.messageService.showInfo(res.msg)
+    //               this.router.navigate(['/auth/signup'])
+    //               return
+    //             }
+    //             userData = this.authService.getUserData('user')
+    //             let bankId = { ...userData, businessBankId: res.data.businessBankId }
+    //             this.authService.setUserData('user', bankId)
+    //             if (res.data.businessKycId.length != 3) {
+    //               this.messageService.showInfo(res.msg)
+    //               this.router.navigate(['/auth/signup'])
+    //               return
+    //             }
+
+    //           }
+
+    //         }
+
+    //         // else {
+    //         //   if (res.data.isRegistrationComplete && res.data.isApproved) {
+    //         //     this.authService.setUserData("user" , res.data)
+    //         //     this.authService.setUserData("accessToken", res.tokens.accessToken)
+    //         //     this.authService.setUserData("refreshToken", res.tokens.refreshToken)
+    //         //     this.authService.setUserData("userId", res.data.userId)
+    //         //     this.authService.setUserData("businessId", res.data.businessId)
+    //         //     this.authService.setUserData("bankId", res.data.businessBankId)
+    //         //     this.authService.setUserData("kycId", res.data.businessKycId[0].kycId)
+    //         //     this.authService.setUserData("usertype", res.data.userType)
+    //         //     this.router.navigate(['/dashboard/merchant-dashboard'])
+    //         //     return
+    //         //   }
+    //         //   if (!res.data.isApproved) {
+    //         //     this.authService.setUserData("user" , res.data)
+    //         //     this.authService.setUserData("businessId", res.data.businessId)
+    //         //     this.authService.setUserData("userId", res.data.userId)
+    //         //     this.authService.setUserData("bankId", res.data.businessBankId)
+    //         //     if (res.data.businessKycId.length != 0) {
+    //         //       this.authService.setUserData("kycId", res.data.businessKycId[0].kycId)
+    //         //     }
+    //         //     this.router.navigate(['/auth/signup/status'])
+    //         //     return
+    //         //   }
+    //         // }
+
+    //       }).catch((err) => {
+    //         console.log(err);
+    //         this.messageService.showError(err.msg)
+    //       })
+
+    //     }
+
+    //   }
 }
