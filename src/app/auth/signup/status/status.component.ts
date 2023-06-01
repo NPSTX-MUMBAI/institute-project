18f00615b66acb97f235b3fa496a4e698a226390
ShivaniS1999 <rsinghshivani55@gmail.com>
2023-06-01 13:09:00 +0530

    fix:bank-info

--- src/app/auth/signup/status/status.component.ts
+++ src/app/auth/signup/status/status.component.ts
@@ -1,18 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Message } from 'primeng/api';
 
 @Component({
-  selector: 'app-status',
-  templateUrl: './status.component.html',
-  styleUrls: ['./status.component.scss']
+    selector: 'app-status',
+    templateUrl: './status.component.html',
+    styleUrls: ['./status.component.scss'],
 })
-export class StatusComponent {
+export class StatusComponent implements OnInit {
+    messages1!: Message[];
+
+    messages2!: Message[];
 
     constructor(private router: Router) {}
+    ngOnInit() {
+        this.messages1 = [
+            {
+                severity: 'success',
+                detail: 'Personal Details',
+            },
+            {
+                severity: 'success',
+                detail: 'Institute Details',
+            },
+        ];
+
+        this.messages2 = [
+            {
+                severity: 'warn',
+                detail: 'Bank Details',
+            },
+            {
+                severity: 'warn',
+                detail: 'Kyc Details',
+            },
+        ];
+    }
 
     route() {
         this.router.navigate(['admin-dashboard']);
     }
-
-
 }
