ecd3b68e5fa164edb8a6ac65abf5f5157ecab704
ShivaniS1999 <rsinghshivani55@gmail.com>
2023-06-01 17:19:36 +0530

    fix:status

--- src/app/auth/signup/status/status.component.ts
+++ src/app/auth/signup/status/status.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Message } from 'primeng/api';
+import { StatusService } from 'src/app/services/status.service';
+import { StateService } from 'src/app/services/state.service';
 
 @Component({
     selector: 'app-status',
@@ -12,32 +14,78 @@ export class StatusComponent implements OnInit {
 
     messages2!: Message[];
 
-    constructor(private router: Router) {}
-    ngOnInit() {
-        this.messages1 = [
-            {
-                severity: 'success',
-                detail: 'Personal Details',
-            },
-            {
-                severity: 'success',
-                detail: 'Institute Details',
-            },
-        ];
+    instStatus = false;
 
-        this.messages2 = [
-            {
-                severity: 'warn',
-                detail: 'Bank Details',
-            },
-            {
-                severity: 'warn',
-                detail: 'Kyc Details',
-            },
-        ];
+    bankStatus = false;
+    kycStatus = false;
+
+    constructor(
+        private router: Router,
+        private statusSvc: StatusService,
+        private stateSvc: StateService
+    ) {}
+    ngOnInit() {
+        // this.messages2 = [
+        //     {
+        //         severity: 'warn',
+        //         detail: 'Bank Details',
+        //     },
+        //     {
+        //         severity: 'warn',
+        //         detail: 'Kyc Details',
+        //     },
+        // ];
+    }
+
+    checkStatus() {
+        let data = {
+            userId: '3ef8ee0751a09c9f8f68',
+            schoolId: '3133a7065cd763f64314',
+        };
+        this.statusSvc.schoolStatus(data).then((res: any) => {
+            console.log(res);
+
+            this.instStatus = res.schoolStatus;
+            this.bankStatus = res.bankStatus;
+            this.kycStatus = res.kycDone.length === 4;
+
+            console.log(this.instStatus, this.bankStatus, this.kycStatus);
+
+            if (this.instStatus == true) {
+                this.messages1 = [
+                    {
+                        severity: 'success',
+                        detail: 'Personal Details',
+                    },
+                ];
+            } else {
+                this.messages1 = [
+                    {
+                        severity: 'warn',
+                        detail: 'Personal Details',
+                    },
+                ];
+            }
+
+            if (this.bankStatus == true) {
+                this.messages2 = [
+                    {
+                        severity: 'success',
+                        detail: 'Bank Details',
+                    },
+                ];
+            } else {
+                this.messages2 = [
+                    {
+                        severity: 'warn',
+                        detail: 'Bank Details',
+                    },
+                ];
+            }
+        });
     }
 
     route() {
-        this.router.navigate(['admin-dashboard']);
+        this.router.navigate(['auth/login']);
     }
 }
