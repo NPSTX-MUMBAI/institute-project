eafc6c66da5cc2da647fbb4c7e61aacf69b858bb
ShivaniS1999 <rsinghshivani55@gmail.com>
2023-06-06 13:12:30 +0530

    fix:signup /personal-info

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,29 +1,58 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
 import { MessageService } from 'primeng/api';
 import { StateService } from '../../services/state.service';
 import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
     selector: 'app-signup',
     templateUrl: './signup.component.html',
     styleUrls: ['./signup.component.scss'],
 })
-export class SignupComponent {
+export class SignupComponent implements OnInit {
     activeIndex: number = 0;
     userData: any;
     personalInfoComplete: boolean = false;
     businessInfoComplete: boolean = false;
     bankInfoComplete: boolean = false;
     kycInfoComplete: boolean = false;
+    internetStatus: any;
 
     constructor(
         private auth: AuthService,
         private messageService: MessageService,
         private stateSvc: StateService,
-        private router: Router
+        private router: Router,
+        private http: HttpClient
     ) {}
-
+    ngOnInit(): void {
+        this.checkInternetConnection();
+    }
+    checkInternetConnection() {
+        const url = 'https://dns.google.com/resolve';
+        this.http.get(url).subscribe(
+            (e) => {
+                setTimeout(() => {
+                    alert('Hello...');
+                }, 5000);
+                console.log(e);
+            },
+            (error: any) => {
+                this.internetStatus = error.status;
+                console.log('error is', error.status);
+                if (error.status == 0) {
+                    this.messageService.add({
+                        severity: 'warn',
+                        // summary: 'warn',
+                        detail: 'Check internet connection',
+                    });
+                } else {
+                    console.log('shivaniiiiiiiiiiiiiiiiiiiiiiiiii');
+                }
+            }
+        );
+    }
     async onSavePersonalInfo(event: any) {
         console.log(event, 'child event 1');
 
